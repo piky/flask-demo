22513dd3a3e4035c755b645711a319913d1e816f
Wijai Thongsom <khainui@gmail.com>
2021-09-27 17:28:48 +0700

    Remove log settings

--- settings.py
+++ settings.py
@@ -1,10 +1,4 @@
 import os
-import logging as log
-
-# Log settings
-LOG_LEVEL = os.environ.get("LOG_LEVEL", "warning").upper()
-
-log.basicConfig(level=LOG_LEVEL)
 
 # Database authentication settings
 DB_PASSWORD = os.environ.get("MYSQL_ROOT_PASSWORD")
