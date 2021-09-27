ee2e1087c5d2f785772881cc87323fd98931e139
Wijai Thongsom <khainui@gmail.com>
2021-09-27 19:09:17 +0700

    Revert "Fix app config"
    This reverts commit 010a414f9688c348c7a1a43b9448c3aab4c538c3.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask
 from . import settings
 
 app = Flask(__name__)
-app.config.from_object("flask-demo.settings")
+app.config.from_object("app.settings")
 
 @app.route('/')
 def hello_world():
