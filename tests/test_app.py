32671ec27c683091778c2420061b2ef4819cb221
Wijai Thongsom <khainui@gmail.com>
2023-10-12 20:24:22 +0700

    Incorrect assert

--- tests/test_app.py
+++ tests/test_app.py
@@ -10,4 +10,4 @@ def test_index_route():
     response = app.test_client().get('/')
 
     assert response.status_code == 200
-    assert response.data.decode('utf-8') == 'Hello, Python!'
+    assert response.data.decode('utf-8') == 'Hello, Flask!'
